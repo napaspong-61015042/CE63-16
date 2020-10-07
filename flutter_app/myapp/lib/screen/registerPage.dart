d44195ec2eec501b9060bb9ea0d5097f8896e68f
Pawanrawee Mitmusik <64927155+Pawanrawee-61015050@users.noreply.github.com>
2020-10-08 02:24:11 +0700

    Edit register page
    แก้ไขหน้า register

--- flutter_app/myapp/lib/screen/registerPage.dart
+++ flutter_app/myapp/lib/screen/registerPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'color_palette.dart';
 import 'icon_data.dart';
+import 'home.dart';
+import 'SecondPage.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -15,13 +17,13 @@ Widget nameText() {
           color: Colors.white,
           size: 36.0,
         ),
-        labelText: 'Display Name : ',
+        labelText: 'Name : ',
         labelStyle: TextStyle(
           color: Colors.white,
           fontWeight: FontWeight.bold,
           fontSize: 20.0,
         ),
-        helperText: 'Type your Name for Display',
+        helperText: 'Type your Firstname and Lastname',
         helperStyle: TextStyle(
           color: Colors.white,
           fontSize: 14.0,
@@ -67,7 +69,7 @@ Widget passwordText() {
           fontWeight: FontWeight.bold,
           fontSize: 20.0,
         ),
-        helperText: 'Type your Password',
+        helperText: 'Type your Password more than 6 character',
         helperStyle: TextStyle(
           color: Colors.white,
           fontSize: 14.0,
@@ -76,15 +78,144 @@ Widget passwordText() {
   );
 }
 
+Widget idcardText() {
+  return TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(
+          Icons.credit_card,
+          color: Colors.white,
+          size: 36.0,
+        ),
+        labelText: 'ID card number : ',
+        labelStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        helperText: 'Type your ID card number 13 digits',
+        helperStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        )),
+  );
+}
+
+Widget telephoneText() {
+  return TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(
+          Icons.phone,
+          color: Colors.white,
+          size: 36.0,
+        ),
+        labelText: 'Telephone Number : ',
+        labelStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        helperText: 'Type your Telephone number 10 digits',
+        helperStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        )),
+  );
+}
+
+Widget motorcyclebrandsText() {
+  return TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(
+          Icons.motorcycle,
+          color: Colors.white,
+          size: 36.0,
+        ),
+        labelText: 'Motorcycle Brands : ',
+        labelStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        helperText: 'Ex. Honda Click, Yamaha Fino, Honda Zoomer-X',
+        helperStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        )),
+  );
+}
+
+Widget motorcyclelicenseplateText() {
+  return TextFormField(
+    decoration: InputDecoration(
+        icon: Icon(
+          Icons.web,
+          color: Colors.white,
+          size: 36.0,
+        ),
+        labelText: 'Motorcycle license plate : ',
+        labelStyle: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+        helperText: 'Type your Motorcycle license plate',
+        helperStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 14.0,
+          fontStyle: FontStyle.italic,
+        )),
+  );
+}
+
+Widget registerButton() {
+  return Container(
+    child: Padding(
+      padding: EdgeInsets.all(30.0),
+      child: MaterialButton(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24.0),
+        ),
+        onPressed: () {},
+        child:
+//          isloading ? CupertinoActivityIndicator() :
+            Text('Register'),
+      ),
+    ),
+  );
+}
+
+Widget loginButton() {
+  return Container(
+    child: Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: FlatButton(
+        onPressed: () {
+//          Navigator.push(context,
+//              MaterialPageRoute(builder: (context) => SecondPage()));
+        },
+        child: Text('Already a User? Login'),
+        textColor: Colors.white,
+      ),
+    ),
+  );
+}
+
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: ColorPalette.grey60,
+      backgroundColor: ColorPalette.grey90,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0.0,
-        title: Text('Register'),
+        title: Text(
+          'Register',
+          textAlign: TextAlign.center,
+        ),
       ),
       body: ListView(
         padding: EdgeInsets.all(30.0),
@@ -92,6 +223,12 @@ class _RegisterPageState extends State<RegisterPage> {
           nameText(),
           emailText(),
           passwordText(),
+          idcardText(),
+          telephoneText(),
+          motorcyclebrandsText(),
+          motorcyclelicenseplateText(),
+          registerButton(),
+          loginButton(),
         ],
       ),
     );
