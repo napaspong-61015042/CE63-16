f7bcb84c8a624764f9523c8c75f76859708d18b6
Napaspong Wiphanngern <71384404+napaspong-61015042@users.noreply.github.com>
2020-10-08 19:06:15 +0700

    Update Esp_send.ino

--- Basestation/Esp_send/Esp_send.ino
+++ Basestation/Esp_send/Esp_send.ino
@@ -1,35 +1,53 @@
-#include <WiFi.h>
 #include <esp_now.h>
+#include <WiFi.h>
 
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  String dataIn;
-  for (int i = 0; i < len; i++) {
-    dataIn += (char)incomingData[i];
-  }
+uint8_t broadcastAddress[] = {0x24, 0x0A, 0xC4, 0xAA, 0x10, 0xD4};
 
-  Serial.print("Data in: ");
-  Serial.println(dataIn);
-}
+
+/********************************************************************************/
+
 
 void setup() {
+
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
 
-  esp_now_init();
-  esp_now_register_recv_cb(OnDataRecv);
+  Serial.println();
+  Serial.println(WiFi.macAddress());
 
-  static esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, new uint8_t[6] { 0x24, 0x0A, 0xC4, 0xAA, 0x14, 0x94 }, 6);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // register peer
+  esp_now_peer_info_t peerInfo;
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
+
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // send data
+  int x = 20;
+  String data = "Hello_Board_1";
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)data.c_str(), data.length() );
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(1000);
 
 }
