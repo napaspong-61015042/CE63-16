268d5df1ccfc220a24d574373f6b8c5cbcc0ae68
Napaspong Wiphanngern <71384404+napaspong-61015042@users.noreply.github.com>
2021-02-04 14:29:56 +0700

    Firebase
    Firebase v.1

---
+++ Basestation/firebase/Sample/Sample.ino
@@ -0,0 +1,316 @@
+#include <esp_now.h>
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
+
+#define FIREBASE_HOST "https://motorcycle-theft.firebaseio.com/"
+#define FIREBASE_AUTH "6Ww8PReofVxLpFuUgHLkXP4piIhqrlsaMYlfnVIk"
+#define WIFI_SSID "raspiwifi"
+#define WIFI_PASSWORD "ohm12515"
+FirebaseData firebaseData;
+
+
+FirebaseJson json;
+unsigned long sendDataPrevMillis = 0;
+
+String path = "/Test/MOTORCYCLE";
+
+uint16_t count = 0;
+void printResult(FirebaseData &data);
+
+String dataReceive;
+String macAddressId;
+void onReceiveData(const uint8_t *mac, const uint8_t *Indata, int len) {
+
+  Serial.print("Received from MAC: ");
+
+  for (int i = 0; i < 6; i++) {
+
+    Serial.printf("%02X", mac[i]);
+    if (i < 5)Serial.print(":");
+  }
+
+  Serial.println();
+  String dataIn;
+  for (int i = 0; i < len; i++) {
+    dataIn += (char)Indata[i];
+  }
+  dataReceive = dataIn;
+  Serial.print("Datain:");
+  //Serial.println(dataIn);
+  char text[100];
+  dataReceive.toCharArray(text, 100);
+  
+  if (macAddressId.length() < 17) {
+    for (int i = 1; i <= 17; i++) {
+      macAddressId += text[i];
+    }
+  }
+  
+  Serial.println(macAddressId);
+
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(onReceiveData);
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set database read timeout to 1 minute (max 15 minutes)
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  //tiny, small, medium, large and unlimited.
+  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
+
+  //optional, set the decimal places for float and double data to be stored in database
+  Firebase.setFloatDigits(2);
+  Firebase.setDoubleDigits(6);
+
+  if (!Firebase.beginStream(firebaseData, path))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't begin stream connection...");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+
+
+}
+
+void printResult(FirebaseData &data)
+{
+
+  if (data.dataType() == "int")
+    Serial.println(data.intData());
+  else if (data.dataType() == "float")
+    Serial.println(data.floatData(), 5);
+  else if (data.dataType() == "double")
+    printf("%.9lf\n", data.doubleData());
+  else if (data.dataType() == "boolean")
+    Serial.println(data.boolData() == 1 ? "true" : "false");
+  else if (data.dataType() == "string")
+    Serial.println(data.stringData());
+  else if (data.dataType() == "json")
+  {
+    Serial.println();
+    FirebaseJson &json = data.jsonObject();
+    //Print all object data
+    Serial.println("Pretty printed JSON data:");
+    String jsonStr;
+    json.toString(jsonStr, true);
+    Serial.println(jsonStr);
+    Serial.println();
+    Serial.println("Iterate JSON data:");
+    Serial.println();
+    size_t len = json.iteratorBegin();
+    String key, value = "";
+    int type = 0;
+    for (size_t i = 0; i < len; i++)
+    {
+      json.iteratorGet(i, type, key, value);
+      Serial.print(i);
+      Serial.print(", ");
+      Serial.print("Type: ");
+      Serial.print(type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+      if (type == FirebaseJson::JSON_OBJECT)
+      {
+        Serial.print(", Key: ");
+        Serial.print(key);
+      }
+      Serial.print(", Value: ");
+      Serial.println(value);
+    }
+    json.iteratorEnd();
+  }
+  else if (data.dataType() == "array")
+  {
+    Serial.println();
+    //get array data from FirebaseData using FirebaseJsonArray object
+    FirebaseJsonArray &arr = data.jsonArray();
+    //Print all array values
+    Serial.println("Pretty printed Array:");
+    String arrStr;
+    arr.toString(arrStr, true);
+    Serial.println(arrStr);
+    Serial.println();
+    Serial.println("Iterate array values:");
+    Serial.println();
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+      Serial.print(i);
+      Serial.print(", Value: ");
+
+      FirebaseJsonData &jsonData = data.jsonData();
+      //Get the result data from FirebaseJsonArray object
+      arr.get(jsonData, i);
+      if (jsonData.typeNum == FirebaseJson::JSON_BOOL)
+        Serial.println(jsonData.boolValue ? "true" : "false");
+      else if (jsonData.typeNum == FirebaseJson::JSON_INT)
+        Serial.println(jsonData.intValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_FLOAT)
+        Serial.println(jsonData.floatValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_DOUBLE)
+        printf("%.9lf\n", jsonData.doubleValue);
+      else if (jsonData.typeNum == FirebaseJson::JSON_STRING ||
+               jsonData.typeNum == FirebaseJson::JSON_NULL ||
+               jsonData.typeNum == FirebaseJson::JSON_OBJECT ||
+               jsonData.typeNum == FirebaseJson::JSON_ARRAY)
+        Serial.println(jsonData.stringValue);
+    }
+  }
+  else if (data.dataType() == "blob")
+  {
+
+    Serial.println();
+
+    for (int i = 0; i < data.blobData().size(); i++)
+    {
+      if (i > 0 && i % 16 == 0)
+        Serial.println();
+
+      if (i < 16)
+        Serial.print("0");
+
+      Serial.print(data.blobData()[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  else if (data.dataType() == "file")
+  {
+
+    Serial.println();
+
+    File file = data.fileStream();
+    int i = 0;
+
+    while (file.available())
+    {
+      if (i > 0 && i % 16 == 0)
+        Serial.println();
+
+      int v = file.read();
+
+      if (v < 16)
+        Serial.print("0");
+
+      Serial.print(v, HEX);
+      Serial.print(" ");
+      i++;
+    }
+    Serial.println();
+    file.close();
+  }
+  else
+  {
+    Serial.println(data.payload());
+  }
+}
+
+void loop() {
+
+
+  if (millis() - sendDataPrevMillis > 15000)
+  {
+    sendDataPrevMillis = millis();
+    count++;
+
+    Serial.println("------------------------------------");
+    Serial.println("Set string...");
+    //if (Firebase.setString(firebaseData, path + "/String", "Hello World! " + String(count)))
+    if (Firebase.setString(firebaseData, path + "/MacAdress_BASE Station", macAddressId))
+    {
+      Serial.println("PASSED");
+      Serial.println("PATH: " + firebaseData.dataPath());
+      Serial.println("TYPE: " + firebaseData.dataType());
+      Serial.print("VALUE: ");
+      printResult(firebaseData);
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + firebaseData.errorReason());
+      Serial.println("------------------------------------");
+      Serial.println();
+    }
+
+//    if (Firebase.setString(firebaseData, path + "/STATUS", dataReceive))
+//    {
+//      Serial.println("PASSED");
+//      Serial.println("PATH: " + firebaseData.dataPath());
+//      Serial.println("TYPE: " + firebaseData.dataType());
+//      Serial.print("VALUE: ");
+//      printResult(firebaseData);
+//      Serial.println("------------------------------------");
+//      Serial.println();
+//    }
+//    else
+//    {
+//      Serial.println("FAILED");
+//      Serial.println("REASON: " + firebaseData.errorReason());
+//      Serial.println("------------------------------------");
+//      Serial.println();
+//    }
+    //Stop WiFi client will gain the free memory
+    //This requires more time for the SSL handshake process in the next connection
+    // due to the previous connection was completely stopped.
+    firebaseData.stopWiFiClient();
+
+    Serial.print("FreeHeap: ");
+    Serial.println(ESP.getFreeHeap());
+  }
+
+  if (!Firebase.readStream(firebaseData))
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Can't read stream data...");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+
+  if (firebaseData.streamTimeout())
+  {
+    Serial.println("Stream timeout, resume streaming...");
+    Serial.println();
+  }
+
+  if (firebaseData.streamAvailable())
+  {
+    Serial.println("------------------------------------");
+    Serial.println("Stream Data available...");
+    Serial.println("STREAM PATH: " + firebaseData.streamPath());
+    Serial.println("EVENT PATH: " + firebaseData.dataPath());
+    Serial.println("DATA TYPE: " + firebaseData.dataType());
+    Serial.println("EVENT TYPE: " + firebaseData.eventType());
+    Serial.print("VALUE: ");
+    printResult(firebaseData);
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
